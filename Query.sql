fee5b2fe173426a66c47a269eda0e6bf67c91848
jboenawan <jboenawan1@babson.edu>
2016-10-25 21:20:57 -0400

    CheeseCake Factory

---
+++ Query.sql
@@ -0,0 +1,63 @@
+﻿Use CheesecakeFactoryDB 
+Go
+
+/*1*/
+
+select * 
+from Customer;
+
+select * 
+from Sales;
+
+select * 
+from Dish;
+
+select * 
+from Employee;
+
+select * 
+from Ingredients;
+
+select * 
+from IngredientDish;
+
+select * 
+from Job;
+
+select * 
+from SalesDetail;
+
+select * 
+from Store;
+
+select * 
+from Supplier;
+
+
+/*2*/
+Select SalesDate, max(SalesTotalAmount) as Max_Sales_Total_Amount
+from Sales
+where SalesDate like '2016-08-05 %'
+group by SalesDate;
+
+
+/*3*/
+Select sum(SalesTotalAmount), sa.SalesDate, s.StoreId, s.Address, s.State
+from sales as sa
+inner join Store as s
+on sa.StoreID = s.StoreID
+where s.state = 'Providence'
+group by sa.SalesDate, s.StoreID, s.Address, s.State;
+
+
+/*4*/
+
+
+/*5*/
+select avg(SalesTotalAmount) as Avg_of_TotalAmount from Sales;
+
+select SalesID, SalesTotalAmount
+from Sales
+where SalesTotalAmount > (select avg(SalesTotalAmount) as Avg_of_TotalAmount 
+from Sales)
+order by SalesTotalAmount asc;
